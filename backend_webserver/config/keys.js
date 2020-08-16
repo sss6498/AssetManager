018685684a031fbb1bc834a50ee1fb92a658bea8
Sagar Shah <sss6498@gmail.com>
2020-08-15 22:03:07 -0400

    Update keys.js

--- backend_webserver/config/keys.js
+++ backend_webserver/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://sss6498:PVHSsss16@asset-manager-web-9j73w.mongodb.net/assetManager?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://sss6498:PASSWORD@asset-manager-web-9j73w.mongodb.net/assetManager?retryWrites=true&w=majority",
     secretOrKey: "secret"
   };
